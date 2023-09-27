b05898e8957e9ad0e1addab39c3d4651e512fa0f
Abhinav Gupta <93302416+abhinavmaddheshiya@users.noreply.github.com>
2023-09-27 19:12:27 +0000

    let var const

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
+const accountName="abhinav gupta"
+let accountId=123445
+var accountType="primary"
+//dont use var as it has some scope issues
+accountLoc="mumbai"
+let accountState
+//name="tinku" 
+//not allowed to change const
+accountId=12345
 console.log("abhinav")
+console.table([accountName, accountId, accountType, accountLoc, accountState])
